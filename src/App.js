4ea9c55d623aa4c2e8eb0c1e09ea630b4c14e0a7
shaileechoxi <shaileechoxi@gmail.com>
2020-11-04 16:13:33 -0500

    lottie added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
-import Lottie from 'lottie-web';
+import Lottie from 'lottie-react-web'
 
 function App() {
 
